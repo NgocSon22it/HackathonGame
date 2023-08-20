28898dd95534ab0265da7ea397a5628e420989fa
NgocSon22it <102401522+NgocSon22it@users.noreply.github.com>
2023-08-20 16:29:36 +0700

    up

--- HocVui/Assets/Scripts/Game/GameManager.cs
+++ HocVui/Assets/Scripts/Game/GameManager.cs
@@ -36,28 +36,7 @@ public class GameManager : MonoBehaviourPunCallbacks
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        ConnectServer();
-        StartCoroutine(CountDown_ShowQuestion());
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        PhotonNetwork.JoinLobby();
-        Debug.Log("OnConnectedToMaster");
-    }
-
-    public void ConnectServer()
-    {
-        PhotonNetwork.NickName = References.GenerateRandomString(10);
-        PhotonNetwork.ConnectUsingSettings();
-        PhotonNetwork.AutomaticallySyncScene = true;
-
-        PhotonNetwork.JoinLobby();
-        Debug.Log(PhotonNetwork.NickName + " JoinLobby");
-        Debug.Log("Connect to Server");
-    }
+    
 
     // Update is called once per frame
     void Update()
